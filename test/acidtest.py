e47f39937c8ae8906f6e1f0466ef9a8fceb4f978
Nico Schlömer <nico.schloemer@gmail.com>
2010-05-11 16:48:00 +0200

    * don't import with wildcards (*) * small style changes

--- test/acidtest.py
+++ test/acidtest.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pylab import *
-
 from os import path
 
 import sys
 
-from matplotlib2tikz import *
-from testfunctions import *
+import matplotlib as mpl
 
-# =============================================================================
+import matplotlib2tikz
+import testfunctions as tf
+
+# ==============================================================================
 def acidtest():    
     tex_file_path = "./tex/acid.tex"
-    data_dir = "./data" # directory where all the generated files will end up
-    tex_relative_path_to_data = "../data" # how to get from the LaTeX file to the data
+
+    # directory where all the generated files will end up
+    data_dir = "./data"
+
+    # how to get from the LaTeX file to the data
+    tex_relative_path_to_data = "../data"
 
     figure_width = "5cm"
 
@@ -23,8 +27,15 @@ def acidtest():
 
     write_document_header( file_handle, figure_width )
 
-    test_functions = [ basic_sin, subplots, image_plot,
-                       noise, patches, legends, legends2, logplot, subplot4x4 ]
+    test_functions = [ tf.basic_sin,
+                       tf.subplots,
+                       tf.image_plot,
+                       tf.noise,
+                       tf.patches,
+                       tf.legends,
+                       tf.legends2,
+                       tf.logplot,
+                       tf.subplot4x4 ]
     
     # see if the command line options tell which subset of the
     # tests are to be run
@@ -32,7 +43,7 @@ def acidtest():
     for arg in sys.argv:
         try:
             test_list.append( int(arg) )
-        except:
+        except ValueError:
             pass
     
     if len(test_list)!=0: # actually treat a sublist of test_functions
@@ -51,18 +62,21 @@ def acidtest():
 
         # convert to TikZ
         tikz_path = data_dir + "/test" + repr(k) + ".tikz"
-        matplotlib2tikz( tikz_path,
-                         figurewidth=figure_width, 
-                         tex_relative_path_to_data = tex_relative_path_to_data )
+        matplotlib2tikz.matplotlib2tikz( tikz_path,
+                                         figurewidth=figure_width, 
+                                         tex_relative_path_to_data = \
+                                                     tex_relative_path_to_data )
 
         # plot reference figure
         pdf_path  = data_dir + "/test" + repr(k) + ".pdf"
-        savefig(pdf_path)
+        mpl.pyplot.savefig(pdf_path)
 
         # update the LaTeX file
         write_file_comparison_entry( file_handle,
-                                     path.join( tex_relative_path_to_data,  path.basename(pdf_path) ),
-                                     path.join( tex_relative_path_to_data,  path.basename(tikz_path) ),
+                                     path.join( tex_relative_path_to_data,
+                                                path.basename(pdf_path) ),
+                                     path.join( tex_relative_path_to_data,
+                                                path.basename(tikz_path) ),
                                      k,
                                      comment )
         k = k+1
@@ -71,7 +85,7 @@ def acidtest():
     file_handle.close()
 
     return
-# =============================================================================
+# ==============================================================================
 def write_document_header( file_handle, figure_width ):
     file_handle.write( "\\documentclass{scrartcl}\n"
                        "\\pdfminorversion=5\n"
@@ -86,11 +100,11 @@ def write_document_header( file_handle, figure_width ):
                        "\\begin{document}\n\n"
                      )
     return
-# =============================================================================
+# ==============================================================================
 def write_document_closure( file_handle ):
     file_handle.write( "\\end{document}" )
     return
-# =============================================================================
+# ==============================================================================
 def write_file_comparison_entry( file_handle,
                                  pdf_path,
                                  tikz_path,
@@ -99,16 +113,19 @@ def write_file_comparison_entry( file_handle,
     file_handle.write(   "% test plot " + str(test_id) + "\n"
                        + "\\begin{figure}%\n"
                        + "\\centering%\n"
-                       + "\\subfloat[][Reference PDF figure.]{\includegraphics[width=\\figwidth]"
+                       + "\\subfloat[][Reference PDF figure.]{" \
+                                            "\includegraphics[width=\\figwidth]"
                           + "{" + str(pdf_path) + "}}%\n"
                        + "\\qquad%\n"
-                       + "\\subfloat[][\\texttt{matplotlib2tikz}-generated]{\input{" + str(tikz_path) + "}}%\n"
-                       + "\\caption{" + str(comment) + " (test ID " + str(test_id) + ").}%\n"
+                       + "\\subfloat[][\\texttt{matplotlib2tikz}-generated]{" \
+                                            "\input{" + str(tikz_path) + "}}%\n"
+                       + "\\caption{" + str(comment) + " (test ID " \
+                       + str(test_id) + ").}%\n"
                        + "\\end{figure}\n\n"
                      )
     return
-# =============================================================================
-if __name__=="__main__":
+# ==============================================================================
+if __name__ == "__main__":
     # execute the test
     acidtest()
-# =============================================================================
+# ==============================================================================
