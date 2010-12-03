54ab03c8c4a79cf056d3aad1b87335c78e233b7c
Nico Schlömer <nico.schloemer@gmail.com>
2010-12-03 19:06:23 +0100

    Corrected project name in license statement.

--- matplotlib2tikz.py
+++ matplotlib2tikz.py
@@ -16,7 +16,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# along with matplotlib2tikz.  If not, see <http://www.gnu.org/licenses/>.
 #
 # ==============================================================================
 """Script to convert Matplotlib generated figures into TikZ/Pgfplots figures.
