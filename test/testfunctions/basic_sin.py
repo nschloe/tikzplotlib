ab258f5bb794add333e594e024533aecbdbf6884
Nico Schlömer <nico.schloemer@gmail.com>
2015-11-02 12:39:09 +0100

    change color in basic_sin test

--- test/testfunctions/basic_sin.py
+++ test/testfunctions/basic_sin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 desc = 'Simple $\sin$ plot with some labels'
-phash = '1f34a1cea1cbe1c9'
+phash = '1f34e1cea1cfa1c1'
 
 
 def plot():
@@ -13,7 +13,8 @@ def plot():
         t = np.arange(0.0, 2.0, 0.1)
         s = np.sin(2*np.pi*t)
         s2 = np.cos(2*np.pi*t)
-        plt.plot(t, s, 'o-', lw=4.1)
+        A = plt.cm.jet(np.linspace(0,1,10))
+        plt.plot(t, s, 'o-', lw=4.1, color = A[5])
         plt.plot(t, s2, 'o-', lw=4.1)
         plt.xlabel('time(s)')
         # plt.xlabel('time(s) _ % $ \\')
