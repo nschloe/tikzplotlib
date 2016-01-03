6d6d0a70e41b0b9812036278f65528bcfd122708
Nico Schlömer <nico.schloemer@gmail.com>
2016-01-04 00:47:44 +0100

    explicitly mention python 2, python 3 in categories

--- setup.py
+++ setup.py
@@ -34,6 +34,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python 2',
+        'Programming Language :: Python 3',
         'Topic :: Multimedia :: Graphics :: Graphics Conversion'
         ]
     )
