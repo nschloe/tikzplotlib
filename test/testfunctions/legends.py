a69697e6a5060d0c609a8846d44633e7f9be2d9a
Nico Schlömer <nico.schloemer@gmail.com>
2016-07-29 11:33:27 +0200

    more legend tests

--- test/testfunctions/legends.py
+++ test/testfunctions/legends.py
@@ -5,10 +5,10 @@ phash = 'fdfc810697a10e2b'
 
 
 def plot():
-    from matplotlib import pyplot as pp
+    from matplotlib import pyplot as plt
     import numpy as np
 
-    fig = pp.figure()
+    fig = plt.figure()
 
     x = np.ma.arange(0, 2*np.pi, 0.02)
     y = np.ma.sin(x)
@@ -17,13 +17,14 @@ def plot():
     ym1 = np.ma.masked_where(y1 > 0.5, y1)
     ym2 = np.ma.masked_where(y2 < -0.5, y2)
 
-    lines = pp.plot(x, y, 'r', x, ym1, 'g', x, ym2, 'bo')
-    pp.setp(lines[0], linewidth=4)
-    pp.setp(lines[1], linewidth=2)
-    pp.setp(lines[2], markersize=10)
+    lines = plt.plot(x, y, 'r', x, ym1, 'g', x, ym2, 'bo')
+    plt.setp(lines[0], linewidth=4)
+    plt.setp(lines[1], linewidth=2)
+    plt.setp(lines[2], markersize=10)
 
-    pp.legend(('No mask', 'Masked if > 0.5', 'Masked if < -0.5'),
-              loc='upper right'
-              )
-    pp.title('Masked line demo')
+    plt.legend(
+            ('No mask', 'Masked if > 0.5', 'Masked if < -0.5'),
+            loc='upper right'
+            )
+    plt.title('Masked line demo')
     return fig
--- test/testfunctions/legends2.py
+++ test/testfunctions/legends2.py
@@ -5,9 +5,9 @@ phash = '5f31c1ceb43c2c72'
 
 
 def plot():
-    from matplotlib import pyplot as pp
+    from matplotlib import pyplot as plt
     import numpy as np
-    fig = pp.figure()
+    fig = plt.figure()
 
     t1 = np.arange(0.0, 2.0, 0.1)
     t2 = np.arange(0.0, 2.0, 0.01)
@@ -15,17 +15,25 @@ def plot():
     # note that plot returns a list of lines.  The 'l1, = plot' usage
     # extracts the first element of the list inot l1 using tuple
     # unpacking.  So l1 is a Line2D instance, not a sequence of lines
-    l1,    = pp.plot(t2, np.exp(-t2))
-    l2, l3 = pp.plot(t2, np.sin(2*np.pi*t2), '--go', t1, np.log(1+t1), '.')
-    l4,    = pp.plot(t2, np.exp(-t2)*np.sin(2*np.pi*t2), 'rs-.')
+    l1,    = plt.plot(t2, np.exp(-t2))
+    l2, l3 = plt.plot(t2, np.sin(2*np.pi*t2), '--go', t1, np.log(1+t1), '.')
+    l4,    = plt.plot(t2, np.exp(-t2)*np.sin(2*np.pi*t2), 'rs-.')
 
-    pp.legend(
+    leg1 = plt.legend(
             (l2, l4),
             ('oscillatory', 'damped'),
             loc='upper right',
             shadow=True
             )
-    pp.xlabel('time')
-    pp.ylabel('volts')
-    pp.title('Damped oscillation')
+    plt.gca().add_artist(leg1)
+
+    for loc in range(2, 11):
+        leg = plt.legend((l1,), ('loc %d' % loc,), loc=loc)
+        leg.get_frame().set_facecolor('#00FFCC')
+        leg.get_frame().set_edgecolor('#0000FF')
+        plt.gca().add_artist(leg)
+
+    plt.xlabel('time')
+    plt.ylabel('volts')
+    plt.title('Damped oscillation')
     return fig
