97a97de2c688c9041b07371fca3156257a2d6223
Nico Schlömer <nico.schloemer@gmail.com>
2016-07-29 12:06:33 +0200

    more grid tests

--- test/testfunctions/logplot.py
+++ test/testfunctions/logplot.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 #
 desc = 'Log scaled plot'
-phash = 'a9837a09ee29bfa0'
+phash = 'ab03fa09ee29bf80'
 
 
 def plot():
-    from matplotlib import pyplot as pp
+    from matplotlib import pyplot as plt
 
     a = [pow(10, i) for i in range(10)]
-    fig = pp.figure()
+    fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     line, = ax.semilogy(a, color='blue', lw=2)
+
+    plt.grid(b=True, which='major', color='g', linestyle='-', linewidth=0.25)
+    plt.grid(b=True, which='minor', color='r', linestyle='--', linewidth=0.5)
 
     return fig
