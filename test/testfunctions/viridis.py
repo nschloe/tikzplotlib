f9c791a505ff6508557426b4f4a40c8e3d9db022
Nico Schlömer <nico.schloemer@gmail.com>
2016-09-08 09:21:19 +0200

    add missing file

---
+++ test/testfunctions/viridis.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+desc = 'Viridis color map'
+phash = 'fd7e03fc03ad0381'
+
+
+def plot():
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import matplotlib.cm as cm
+
+    x, y = np.meshgrid(np.linspace(0, 1), np.linspace(0, 1))
+    z = x**2 - y**2
+
+    fig = plt.figure()
+    plt.pcolormesh(x, y, z, cmap=cm.viridis)
+    # plt.colorbar()
+
+    return fig
