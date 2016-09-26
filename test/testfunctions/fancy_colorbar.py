5d5e41d343e9ee77c91858e81597332916c0563d
Nico Schlömer <nico.schloemer@gmail.com>
2016-09-26 11:26:49 +0200

    simplify test

--- test/testfunctions/fancy_colorbar.py
+++ test/testfunctions/fancy_colorbar.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 #
 desc = 'Test colorbar options'
-phash = 'f50f0be941f841f4'
+phash = 'b733c9dc03dc4952'
 
 
 def plot():
     import numpy as np
     import matplotlib.pyplot as plt
 
-    da = np.zeros((512, 640))
-    da[1:100, 1:100] = 1.0
+    da = np.zeros((3, 3))
+    da[:2, :2] = 1.0
 
     fig = plt.figure()
     ax = plt.gca()
 
     im = ax.imshow(da, cmap='viridis')
     plt.colorbar(im, aspect=5, shrink=0.5)
-
-    plt.title('A new figure')
 
     return fig
