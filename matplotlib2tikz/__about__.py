9ef20c39d50cee3e3835659f2da3dac6ee02d898
Nico Schlömer <nico.schloemer@gmail.com>
2017-01-20 13:12:39 +0100

    properly handle new meta data

---
+++ matplotlib2tikz/__about__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#
+__author__ = 'Nico Schlömer'
+__email__ = 'nico.schloemer@gmail.com'
+__copyright__ = 'Copyright (c) 2010-2016, %s <%s>' % (__author__, __email__)
+__credits__ = []
+__license__ = 'MIT License'
+__version__ = '0.5.16'
+__maintainer__ = 'Nico Schlömer'
+__status__ = 'Production'
--- matplotlib2tikz/__init__.py
+++ matplotlib2tikz/__init__.py
@@ -3,13 +3,5 @@
 '''Script to convert Matplotlib generated figures into TikZ/PGFPlots figures.
 '''
 
-__author__ = 'Nico Schlömer'
-__email__ = 'nico.schloemer@gmail.com'
-__copyright__ = 'Copyright (c) 2010-2016, %s <%s>' % (__author__, __email__)
-__credits__ = []
-__license__ = 'MIT License'
-__version__ = '0.5.16'
-__maintainer__ = 'Nico Schlömer'
-__status__ = 'Production'
-
+import matplotlib2tikz.__about__
 from matplotlib2tikz.save import save
