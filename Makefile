bb27a527aa9931446915cdebd541a89391080531
Nico Schlömer <nico.schloemer@gmail.com>
2017-01-20 18:13:28 +0100

    export meta info in the matplotlib2tikz namespace

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=$(shell python -c "import matplotlib2tikz; print(matplotlib2tikz.__about__.__version__)")
+VERSION=$(shell python -c "import matplotlib2tikz; print(matplotlib2tikz.__version__)")
 
 # Make sure we're on the master branch
 ifneq "$(shell git rev-parse --abbrev-ref HEAD)" "master"
--- matplotlib2tikz/__init__.py
+++ matplotlib2tikz/__init__.py
@@ -3,5 +3,15 @@
 '''Script to convert Matplotlib generated figures into TikZ/PGFPlots figures.
 '''
 
-import matplotlib2tikz.__about__
+from matplotlib2tikz.__about__ import (
+        __author__,
+        __email__,
+        __copyright__,
+        __credits__,
+        __license__,
+        __version__,
+        __maintainer__,
+        __status__
+        )
+
 from matplotlib2tikz.save import save
