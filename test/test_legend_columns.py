7077905219cd7e38a5339f6adbaa3116cb3c5a0c
Nico Schlömer <nico.schloemer@gmail.com>
2017-06-16 10:26:50 +0200

    handle legend columns

--- test/test_legend_columns.py
+++ test/test_legend_columns.py
@@ -7,15 +7,15 @@ def plot():
     import numpy as np
     import matplotlib.pyplot as plt
     fig, ax = plt.subplots(figsize=(17, 6))
-    ax.plot(np.array([1, 2, 3, 4, 5]), label='Test 1')
-    ax.plot(np.array([5, 4, 3, 2, 1]), label='Test 2')
+    ax.plot(np.array([1, 5]), label='Test 1')
+    ax.plot(np.array([5, 1]), label='Test 2')
     ax.legend(ncol=2, loc='upper center')
     return fig
 
 
 def test():
     phash = helpers.Phash(plot())
-    assert phash.phash == 'a3267e19cad99998', phash.get_details()
+    assert phash.phash == '8386de99666939a9', phash.get_details()
     return
 
 
