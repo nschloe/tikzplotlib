5791ebc307a5d6c21623892e24ef747978311770
Nico Schlömer <nico.schloemer@gmail.com>
2017-11-01 17:23:01 +0100

    fix image tests

--- test/test_fancybox.py
+++ test/test_fancybox.py
@@ -193,7 +193,7 @@ def plot():
 
 def test():
     phash = helpers.Phash(plot())
-    assert phash.phash == 'dd2325dc23cdd81a', phash.get_details()
+    assert phash.phash == 'dd2325d823cdd85a', phash.get_details()
 
 
 if __name__ == '__main__':
