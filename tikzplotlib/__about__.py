736241dd8d080022c8d68ae5b5dfe578aaa0e042
Nico Schlömer <nico.schloemer@gmail.com>
2019-07-04 11:33:32 +0200

    version bump

--- tikzplotlib/__about__.py
+++ tikzplotlib/__about__.py
@@ -1,6 +1,6 @@
-__author__ = u"Nico Schlömer"
+__author__ = "Nico Schlömer"
 __email__ = "nico.schloemer@gmail.com"
-__copyright__ = u"Copyright (c) 2010-2019, {} <{}>".format(__author__, __email__)
+__copyright__ = "Copyright (c) 2010-2019, {} <{}>".format(__author__, __email__)
 __license__ = "License :: OSI Approved :: MIT License"
-__version__ = "0.7.4"
+__version__ = "0.8.0"
 __status__ = "Development Status :: 5 - Production/Stable"
