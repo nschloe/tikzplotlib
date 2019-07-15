fe6bbe92b6850d0e81eabccb4dc690a0353a505a
Lukas Maxeiner <lukas.maxeiner@tu-dortmund.de>
2019-07-15 17:53:41 +0200

    extended test to cover base case

--- test/test_steps.py
+++ test/test_steps.py
@@ -9,14 +9,16 @@ def plot():
     y2 = np.array([1, 2, 1, 4, 2])
     y3 = np.array([1, 2, 1, 4, 2])
     y4 = np.array([1, 2, 1, 4, 2])
+    y5 = np.array([2, 3, 2, 5, 3])
 
     plt.step(x, y1, "r-")
     plt.step(x, y2, "b--", where="pre")
     plt.step(x, y3, "g-.", where="post")
     plt.step(x, y4, "y:", where="mid")
-    plt.legend(["default", "pre", "post", "mid"])
+    plt.plot(x, y5, "c.",)
+    plt.legend(["default", "pre", "post", "mid", "default"])
 
-    return plt.gcf()
+        return plt.gcf()
 
 
 if __name__ == "__main__":
