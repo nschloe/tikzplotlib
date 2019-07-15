3966a46cc004dd1c06ad1e3e801c9314da13272c
Lukas Maxeiner <lukas.maxeiner@tu-dortmund.de>
2019-07-15 17:27:24 +0200

    used black to change ' to "

--- test/test_steps.py
+++ test/test_steps.py
@@ -10,11 +10,11 @@ def plot():
     y3 = np.array([1, 2, 1, 4, 2])
     y4 = np.array([1, 2, 1, 4, 2])
 
-    plt.step(x, y1, 'r-')
-    plt.step(x, y2, 'b--', where='pre')
-    plt.step(x, y3, 'g-.', where='post')
-    plt.step(x, y4, 'y:', where='mid')
-    plt.legend(['default', 'pre', 'post', 'mid'])
+    plt.step(x, y1, "r-")
+    plt.step(x, y2, "b--", where="pre")
+    plt.step(x, y3, "g-.", where="post")
+    plt.step(x, y4, "y:", where="mid")
+    plt.legend(["default", "pre", "post", "mid"])
 
     return plt.gcf()
 
