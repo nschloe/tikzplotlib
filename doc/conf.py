350b1c4ed444bcfc1c562f1f7daf6325947bd7be
Nico Schlömer <nico.schloemer@gmail.com>
2019-07-30 12:07:46 +0200

    make black

--- doc/conf.py
+++ doc/conf.py
@@ -11,15 +11,16 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
+
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'tikzplotlib'
+project = "tikzplotlib"
 copyright = "2010-2019, Nico Schlömer"
-author = 'Nico Schlömer'
+author = "Nico Schlömer"
 
 # https://packaging.python.org/single_source_version/
 this_dir = os.path.abspath(os.path.dirname(__file__))
@@ -45,12 +46,12 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -58,9 +59,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "alabaster"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
