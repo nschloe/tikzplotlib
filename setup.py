2c44bce38c63c28cd1fb62f98a8249b5bb7f39fe
Nico Schlömer <nico.schloemer@gmail.com>
2020-03-02 11:28:17 +0100

    setup.cfg, pyproject.toml

--- setup.py
+++ setup.py
@@ -1,36 +1,4 @@
-import os
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-# https://packaging.python.org/single_source_version/
-base_dir = os.path.abspath(os.path.dirname(__file__))
-about = {}
-with open(os.path.join(base_dir, "tikzplotlib", "__about__.py"), "rb") as f:
-    exec(f.read(), about)
-
-
-setup(
-    name="tikzplotlib",
-    version=about["__version__"],
-    packages=find_packages(),
-    url="https://github.com/nschloe/tikzplotlib",
-    author=about["__author__"],
-    author_email=about["__email__"],
-    install_requires=["matplotlib >= 1.4.0", "numpy", "Pillow"],
-    description="Convert matplotlib figures into TikZ/PGFPlots",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    license=about["__license__"],
-    python_requires=">=3.6",
-    classifiers=[
-        about["__status__"],
-        about["__license__"],
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
-        "Topic :: Scientific/Engineering :: Visualization",
-    ],
-)
+if __name__ == "__main__":
+    setup()
