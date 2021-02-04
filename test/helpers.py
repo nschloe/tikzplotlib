2ad15dad35f0b0167f11d83e9e120a0363ab15bb
Nico Schlömer <nico.schloemer@gmail.com>
2021-02-04 11:19:00 +0100

    don't assert latex compilation to speed up tests

--- test/helpers.py
+++ test/helpers.py
@@ -31,7 +31,7 @@ def _unidiff_output(expected, actual):
 
 
 def assert_equality(
-    plot, filename, assert_compilation=True, flavor="latex", **extra_get_tikz_code_args
+    plot, filename, assert_compilation=False, flavor="latex", **extra_get_tikz_code_args
 ):
     plot()
     code = tikzplotlib.get_tikz_code(
