42428aab4bae9e19ceaf61f9bc2fd4c0a836eda7
Nico Schlömer <nico.schloemer@gmail.com>
2021-08-10 18:14:58 +0200

    add __init__ to tests

--- tests/test_legends2.py
+++ tests/test_legends2.py
@@ -1,4 +1,4 @@
-from helpers import assert_equality
+from .helpers import assert_equality
 
 
 def plot():
@@ -24,4 +24,3 @@ def plot():
 
 def test():
     assert_equality(plot, __file__[:-3] + "_reference.tex")
-    return
