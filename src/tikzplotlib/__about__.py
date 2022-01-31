b33ad8a0c58b2fd6d2404a63a030346e03820d77
Nico Schlömer <nico.schloemer@gmail.com>
2022-01-31 13:13:53 +0100

    use flit

--- src/tikzplotlib/__about__.py
+++ src/tikzplotlib/__about__.py
@@ -1,10 +1,1 @@
-try:
-    # Python 3.8
-    from importlib import metadata
-except ImportError:
-    import importlib_metadata as metadata
-
-try:
-    __version__ = metadata.version("tikzplotlib")
-except Exception:
-    __version__ = "unknown"
+__version__ = "0.9.17"
